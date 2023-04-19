83da36482b928767e4ac04af29af3589927e1744
joaodemari <119618931+joaodemari@users.noreply.github.com>
2023-04-19 14:20:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+@font-face {
+    font-family: 'Montserrat';
+    src: url('/Montserrat/Montserrat-VariableFont_wght.ttf') format('truetype');
+  }
+
+body{
+    background-image: linear-gradient(230deg, #000000, #003585);
+    height:0;
+    font-family: 'Montserrat';
+}
+
+.imagem{
+    padding-top: 5%;
+    display: flex;
+    justify-content: center;
+}
+
+.container{
+    padding-top: 10%;
+    display: flex;
+    justify-content: center;
+}
+
+.moeda1 {
+    display: grid;
+    margin-right: 10%;
+    background-color: white;
+    border-radius: 30px;
+    
+
+}
+
+.moeda2 {
+    display: grid;
+    margin-left: 10%;
+    background-color: white;
+}
+
+.container select {
+    border: 1px solid #dedede;
+    font-size: 16px;
+    background: transparent;
+    background-position: right 10px top 50%, 0, 0;
+    background-size: 12px auto, 100%;
+    border-radius: 100px; 
+    margin: 10px;
+    font-family: 'Montserrat';
+    width: 200px;
+  }
