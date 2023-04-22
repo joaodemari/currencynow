d450b8ff019c1d45378654cde095433872ae6ad7
joaodemari <119618931+joaodemari@users.noreply.github.com>
2023-04-22 16:37:39 -0300

    javascrip final

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const trocar = document.getElementById('trocar');
 
 
 // fetch currency rates and update the dom
-function calcular(){
+function calcular(calculoQuantUm = true){
     const moeda_um = moedaEl_um.value
     const moeda_dois = moedaEl_dois.value
     var quant_um = Number(quantEl_um.value)
@@ -20,38 +20,29 @@ function calcular(){
         .then(data => {
             const taxa = data.conversion_rates[moeda_dois]
             taxaEl.innerText = `1 ${moeda_um} = ${taxa} ${moeda_dois}`;
-            quantEl_dois.value = (quant_um*taxa).toFixed(2);
-        });
-    }
+            if (calculoQuantUm) {
+                quantEl_dois.value = (quant_um*taxa).toFixed(2);
+            } else {
+                quantEl_um.value = (quant_dois/taxa).toFixed(2);
+            }
+        });   
+}
 
-function calcular2(){
-    const moeda_um = moedaEl_um.value
-    const moeda_dois = moedaEl_dois.value
-    var quant_um = Number(quantEl_um.value)
-    var quant_dois = Number(quantEl_dois.value)
-
-    fetch(`https://v6.exchangerate-api.com/v6/65b3eaf4fe2a4c970d24a412/latest/${moeda_um}`)
-        .then(res => res.json())
-        .then(data => {
-            const taxa = data.conversion_rates[moeda_dois]
-            taxaEl.innerText = `1 ${moeda_um} = ${taxa} ${moeda_dois}`;
-            quantEl_um.value = (quant_dois/taxa).toFixed(2);
-        });
-    }
-
-
-
-
-moedaEl_um.addEventListener('change', calcular);
-moedaEl_dois.addEventListener('change', calcular2);
-quantEl_um.addEventListener('input', calcular);
-quantEl_dois.addEventListener('input', calcular2);
+moedaEl_um.addEventListener('change', () => calcular(true));
+moedaEl_dois.addEventListener('change', () => calcular(false));
+quantEl_um.addEventListener('input', () => calcular(true));
+quantEl_dois.addEventListener('input', () => calcular(false));
 
 trocar.addEventListener('click', () => {
     const temp = moedaEl_um.value;
     moedaEl_um.value = moedaEl_dois.value;
     moedaEl_dois.value = temp;
-    calcular();
+    calcular(true);
 })
 
-calcular();
+calcular(true);
+
+
+
+
+
